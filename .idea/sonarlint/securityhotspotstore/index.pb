3e35220595f6782887b038f12f60ee8e2efa3960
Guillo7799 <guillermo.rivera@epn.edu.ec>
2025-02-04 21:43:48 -0500

    Ejercicios de impresión en consola

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -12,3 +12,13 @@ Q
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+L
+src/EjercicioVariables1.java,8\5\85c2dadf7995b2e5db43fc43e7f73605e7d71a46
+L
+src/EjercicioVariables2.java,e\4\e49953474ea74bd9c59322c021b82d994fe7fd5f
+L
+src/EjercicioConstantes.java,4\4\441020bd4331d41b38d9c6d6fedcb6d001f42649
+S
+#src/EjercicioMensajesDatosSout.java,e\d\ed8be26273a644a62c08572cee4617a6d7f3f55b
+H
+src/EjercicioPrintf.java,1\b\1bcca0cef4cd621deb8c181be8a7eb61627b7837
---
+++ src/EjercicioConstantes.java
@@ -0,0 +1,16 @@
+public class EjercicioConstantes {
+    public static void main(String[] args) {
+        final var profesion = "Abogado";
+        final var cantidadJuiciosGanados = 3;
+        final var letraFavorita = 'C';
+        final var peso = 80.52;
+        final var esColegiado = false;
+
+        //después de colocar que son constantes ya no se puede cambiar su valor
+        System.out.println(profesion);
+        System.out.println(cantidadJuiciosGanados);
+        System.out.println(letraFavorita);
+        System.out.println(peso);
+        System.out.println(esColegiado);
+    }
+}
